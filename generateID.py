002d4defc4d83a66697b1a7c0f4b73b7a9060658
iamjaspreetsingh <jsk1961998@gmail.com>
2019-01-15 16:53:50 +0530

    generate your Ids

--- generateID.py
+++ generateID.py
@@ -1,23 +1,30 @@
 import numpy as np
 import cv2
+import os
 
-
-palm_cascade = cv2.CascadeClassifier(r'/home/jaspreet/Desktop/haar-face.xml')
+face_cascade = cv2.CascadeClassifier(r'/home/jaspreet/Desktop/haar-face.xml')
 cap = cv2.VideoCapture(0)
+   
+def release():
+    cap.release()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 while(True):
     ret, frame = cap.read()
     cv2.imshow("frame",frame)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    palmy = palm_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in palmy:
+    facy = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in facy:
         frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
         faces = frame[y:y+h,x:x+w]
-	cv2.imwrite( "/home/jaspreet/Desktop//images/1", faces);
+	path, dirs, files = next(os.walk("/home/jaspreet/Desktop/images"))
+	new_img_ID = len(files)	
+	cv2.imwrite( "/home/jaspreet/Desktop/images/"+str(new_img_ID)+".jpg", faces);
         #print(img)
-        cv2.imshow('frame',faces)
+        cv2.imshow('ID',faces)
+	print ("ID generated :"+ str(new_img_ID))
+	release()
     if cv2.waitKey(1) & 0xFF == ord('h'):
         break
-   
-cap.release()
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
